7ddd7e25d9e6c6ac64518a882e4a08b50be7d5d5
ECManning <31044186+ECManning@users.noreply.github.com>
2017-08-17 13:32:10 -0500

    Update WeatherReport.java

--- WeatherReport.java
+++ WeatherReport.java
@@ -1,11 +1,38 @@
+package weather_report;
+
 import java.util.Scanner;
 
-public class WeatherReport
+public class Weather_report 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
+		Scanner cin = new Scanner(System.in);
 		
+		int temp = cin.nextInt();
+		
+		double ans = (5*((double)temp-32))/9; 
+		
+		System.out.println("Welcome. Please enter your temperature in degrees F.");
+		
+		
+		System.out.println("The temperature in your are is " + ans + " degrees C. Thank you very much have a nice day!");
+		
+		if(temp<=32)
+		{
+			System.out.println("Today it is cold outside.");
+		}
+		
+		
+		if(temp>=90)
+		{
+			System.out.println("Today it is hot outside.");
+		}
+		
+		
+		
+		cin.close();
 	}
-
+	
+	
 }
